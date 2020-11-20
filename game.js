06acab85274ff33f5f63d57bf2b5a4914d62cff7
Tim Phillips <timmyiscool8@gmail.com>
2020-11-19 20:23:50 -0700

    Added tokens with positioning.

--- game.js
+++ game.js
@@ -2,17 +2,30 @@
 // Initial variables.
 let currentTurn = 0;
 
+// Adjust player count.
 let playercount = sessionStorage.getItem('playercount');
 if (playercount == null) {
     playercount = 2;
 }
 
+// Grab player data.
 let pdata = sessionStorage.getItem('pdata').split(",");
+console.log(sessionStorage.getItem('pdata'))
 
+// Pick random player to go first.
 let whoseTurn = Math.ceil(Math.random() * playercount);
 
+// Create player data string.
 let playerdata = {};
 
+// Actually run the game setup.
+setupGame();
+
+// Take first turn.
+takeTurn(whoseTurn);
+
+
+// GAME FUNCTIONS BELOW;
 // Setup game.
 function setupGame() {
     // For each player, give them money and basic variables.
@@ -37,12 +50,9 @@ function setupGame() {
     // Create tokens.
     for (let i = 1; i <= playercount; i++) {
         let board = document.getElementById("board");
-        board.innerHTML += `<img src="images/tokens/${playerdata[i].token}.svg" alt="P${i}" class="token" id="player${i}token">`;
+        board.innerHTML += `<div alt="P${i}" class="token ${playerdata[i].token}" id="player${i}token">P${i}</div>`;
     }
 }
-
-// Actually run the game setup.
-setupGame();
 
 // Bill html generation.
 function gimmeMoney(denom=1, activeBill) {
@@ -135,22 +145,216 @@ function displayBills(player=1) {
     }
 }
 
-displayBills(1);
-
-// Draw players.
-function movePlayer(player=1, position=0) {
-
+// Roll dice.
+function rollDice() {
+    // Generate a random number from 1 to 6.
+    let roll = Math.ceil(Math.random() * 6);
+    return roll;
 }
 
+// Calculate player position.
+function calcPos(player=1) {
+    // Grab the token and data.
+    let position = playerdata[player].pos;
+    let token = document.getElementById(`player${player}token`);
+
+    // Check the data.
+    playerPosCheck(player);
+
+    // Set position.
+    // SIMPLIFY:
+    switch (position) {
+        case 0: 
+            token.style = "top: 93%; left: 93%;";
+            break;
+        case 1:
+            token.style = "top: 93%; left: 81.4%;";
+            break;
+        case 2: 
+            token.style = "top: 93%; left: 73%;";
+            break;
+        case 3: 
+            token.style = "top: 93%; left: 64.7%;";
+            break; 
+        case 4: 
+            token.style = "top: 93%; left: 56.4%;";
+            break; 
+        case 5:
+            token.style = "top: 93%; left: 48%;";
+            break; 
+        case 6: 
+            token.style = "top: 93%; left: 39.9%;";
+            break; 
+        case 7:
+            token.style = "top: 93%; left: 30.9%;";
+            break; 
+        case 8: 
+            token.style = "top: 93%; left: 23%;";
+            break; 
+        case 9: 
+            token.style = "top: 93%; left: 14.5%;";
+            break; 
+        case 10:
+            token.style = "top: 100%; left: 0%;";
+            break; 
+        case 11:
+            token.style = "top: 81%; left: 3%;";
+            break; 
+        case 12:
+            token.style = "top: 73%; left: 3%;";
+            break; 
+        case 13:
+            token.style = "top: 64.7%; left: 3%;";
+            break; 
+        case 14: 
+            token.style = "top: 56.4%; left: 3%;";
+            break; 
+        case 15: 
+            token.style = "top: 48%; left: 3%;";
+            break; 
+        case 16: 
+            token.style = "top: 39.9%; left: 3%;";
+            break; 
+        case 17: 
+            token.style = "top: 30.9%; left: 3%;";
+            break; 
+        case 18:
+            token.style = "top: 23%; left: 3%;";
+            break; 
+        case 19: 
+            token.style = "top: 14.5%; left: 3%;";
+            break;
+        case 20: 
+            token.style = "top: 4%; left: 4%;";
+            break; 
+        case 21: 
+            token.style = "top: 3%; left: 14.5%;";
+            break; 
+        case 22: 
+            token.style = "top: 3%; left: 23%;";
+            break; 
+        case 23:
+            token.style = "top: 3%; left: 30.9%;";
+            break; 
+        case 24: 
+            token.style = "top: 3%; left: 39.9%;";
+            break; 
+        case 25:
+            token.style = "top: 3%; left: 48%;";
+            break; 
+        case 26: 
+            token.style = "top: 3%; left: 56.4%;";
+            break; 
+        case 27: 
+            token.style = "top: 3%; left: 64.7%;";
+            break; 
+        case 28: 
+            token.style = "top: 3%; left: 73%;";
+            break;
+        case 29:
+            token.style = "top: 3%; left: 81.4%;";
+            break;
+        case 30: 
+            token.style = "top: 4%; left: 91.5%;";
+            break; 
+        case 31: 
+            token.style = "top: 14.5%; left: 93%;";
+            break; 
+        case 32:
+            token.style = "top: 23%; left: 93%;";
+            break; 
+        case 33: 
+            token.style = "top: 30.9%; left: 93%;";
+            break; 
+        case 34: 
+            token.style = "top: 39.9%; left: 93%;";
+            break; 
+        case 35: 
+            token.style = "top: 48%; left: 93%;";
+            break; 
+        case 36: 
+            token.style = "top: 56.4%; left: 93%;";
+            break;
+        case 37:
+            token.style = "top: 64.7%; left: 93%;";
+            break;
+        case 37:
+            token.style = "top: 64.7%; left: 93%;";
+            break;
+        case 38:
+            token.style = "top: 73%; left: 93%;";
+            break;    
+        case 39:
+            token.style = "top: 81%; left: 93%;";
+            break;  
+        case "jail":
+            token.style = "top: 90%; left: 6.2%;";
+            break;
+    }
+
+    playerPosCheck(player);
+}
+
+// Reset players position.
+function playerPosCheck(who=1) {
+    // Check if the player is not in jail and is higher than 39, if so go back to 0.
+    if(playerdata[who].pos == 40) {
+        console.log(`PLAYER ${who} NOW: ${playerdata[who].pos}`);
+        playerdata[who].pos = 0;
+        console.log(`PLAYER ${who} NOW: ${playerdata[who].pos}`);
+    }
+}
+
+playerdata[1].pos = 0;
+playerdata[2].pos = 1;
+playerdata[3].pos = 2;
+playerdata[4].pos = 3;
+playerdata[5].pos = 4;
+playerdata[6].pos = 5;
+playerdata[7].pos = 6;
+playerdata[8].pos = 7;
+
+function breakPlayer() {
+    calcPos(1);
+    playerdata[1].pos ++;
+    
+    calcPos(2);
+    playerdata[2].pos ++;
+    
+    calcPos(3);
+    playerdata[3].pos ++;
+    
+    calcPos(4);
+    playerdata[4].pos ++;
+
+    calcPos(5);
+    playerdata[5].pos ++;
+
+    calcPos(6);
+    playerdata[6].pos ++;
+
+    calcPos(7);
+    playerdata[7].pos ++;
+
+    calcPos(8);
+    playerdata[8].pos ++;
+}
+
+window.setInterval(breakPlayer, 300)
+
 async function takeTurn(who=1) {
+    // Move the player on the board.
+    calcPos(who);
+
+    // Display the player's money.
+    displayBills(who);
+
+    // Change player heading.
+    document.getElementById('playerturn').innerText = "Player " + who + "'s Turn"
 
     let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), 1000)
+    setTimeout(() => resolve(displayBills(who)), 1000)
     });
 
     let result = await promise; // wait until the promise resolves (*)
-
-    alert(result); // "done!"
 }
-
-takeTurn();
--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function startGame() {
     } else if (playerCount < 2) { 
         playerCount = 2; 
     }
+
+    pdata = [];
+    for (let i = 1; i <= playerCount; i++) {
+        pdata.push(document.getElementById(`player${i}`).value);
+    }
     
     sessionStorage.setItem('playercount', playerCount);
-    sessionStorage.removeItem('pdata')
+    sessionStorage.removeItem('pdata');
     sessionStorage.setItem('pdata', pdata);
     
     window.location.href='game.html';
@@ -29,47 +34,18 @@ function updateListings() {
     // Add components
     for (let i = 1; i <= playerCount; i++) {
         playerSetup.innerHTML += `
-        <p>Player ${i}'s Piece: <select name="picker" id="player${i}" onchange="updateAvailables();" value="None Picked">
-            <option value="scottie">Scottie</option>
-            <option value="tophat">Top Hat</option>
-            <option value="thimble">Thimble</option>
-            <option value="boot">Boot</option>
-            <option value="wheelbarrow">Wheelbarrow</option>
-            <option value="cat">Cat</option>
-            <option value="racingcar">Racing Car</option>
-            <option value="battleship">Battleship</option>
+        <p>Player ${i}'s Piece: <select name="picker" id="player${i}">
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="purple">Purple</option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
         </select></p>
         `;
     }
-
-    updateAvailables();
 }
 
-// Change what picks are available.
-function updateAvailables() {
-    let players = document.getElementById("playersetup").children;
-
-    let options = ['scottie', 'tophat', 'thimble', 'boot', 'wheelbarrow', 'cat', 'racingcar', 'battleship'];
-
-    for (let activeplayer = 0; activeplayer < players.length; activeplayer++) {
-        for (let otherplayer = 0; otherplayer < players.length; otherplayer++) {
-            if (activeplayer != otherplayer) {
-                let activeModule = players[activeplayer].children[0];
-                let otherModule = players[otherplayer].children[0];
-                while(activeModule.value == otherModule.value) {
-                    otherModule.value = options[Math.floor(Math.random() * options.length)]
-                }
-            } else {
-                continue;
-            }
-        }
-    }
-
-    for (let activeplayer = 0; activeplayer < players.length; activeplayer++) {
-        let activeModule = players[activeplayer].children[0];
-        pdata.push(activeModule.value.toString());
-    }
-}
-
-updateAvailables();
 updateListings();
